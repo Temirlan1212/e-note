0d2929d70f61810422737f6c29cac1dc21d11958
Alymbek Alisher uulu <alymbek1992@gmail.com>
2023-07-11 11:43:00 +0600

    add code formatter

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en', 'ru', 'kg'],
-    defaultLocale: 'ru',
+    locales: ["en", "ru", "kg"],
+    defaultLocale: "ru",
     localeDetection: true,
   },
 };
