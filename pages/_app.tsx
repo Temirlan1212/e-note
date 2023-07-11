813ef0cd655682ac7c32f4c2748dea5cf4445843
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2023-07-11 12:53:49 +0600

    Добавить глобальный стиль

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { NextIntlClientProvider } from 'next-intl';
-import PublicLayout from '@/layouts/Public';
-import PrivateLayout from '@/layouts/Private';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextIntlClientProvider } from "next-intl";
+import PublicLayout from "@/layouts/Public";
+import PrivateLayout from "@/layouts/Private";
+import { ThemeProvider } from "@mui/material";
+import theme from "./globalTheme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextIntlClientProvider messages={pageProps.messages}>
-      <PublicLayout>
-        <Component {...pageProps} />
-      </PublicLayout>
-    </NextIntlClientProvider>
+    <ThemeProvider theme={theme}>
+      <NextIntlClientProvider messages={pageProps.messages}>
+        <PublicLayout>
+          <Component {...pageProps} />
+        </PublicLayout>
+      </NextIntlClientProvider>
+    </ThemeProvider>
   );
 }
---
+++ pages/globalTheme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1BAA75",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
