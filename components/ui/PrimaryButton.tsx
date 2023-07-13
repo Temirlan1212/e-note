05a05906ade62b9d78f9beb24817512514d838c8
wingmeter <nursb189@gmail.com>
2023-07-13 17:56:09 +0600

    feat: добавил enum для переключения между кнопками

--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -3,14 +3,21 @@ import { forwardRef } from "react";
 import Button, { ButtonProps } from "@mui/material/Button";
 import { useTheme } from "@mui/material/styles";
 
-interface PrimaryButtonProps extends ButtonProps {}
+export enum BackgroundColor {
+  Primary,
+  Secondary,
+}
+
+interface PrimaryButtonProps extends ButtonProps {
+  bgColor?: BackgroundColor;
+}
 
 const PrimaryButton: React.ForwardRefRenderFunction<HTMLButtonElement, PrimaryButtonProps> = (props, ref) => {
-  const { variant = "contained", ...rest } = props;
+  const { variant = "contained", bgColor = BackgroundColor.Primary, ...rest } = props;
 
   const theme = useTheme();
 
-  const primaryColor = theme.palette.success.main;
+  const primaryColor = bgColor === BackgroundColor.Primary ? theme.palette.success.main : theme.palette.info.light;
 
   const buttonStyles =
     variant === "contained"
