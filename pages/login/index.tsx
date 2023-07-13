308e382f93ceb4cc7ddb18ea8dc098abf843f024
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2023-07-13 20:43:11 +0600

    Login page

---
+++ pages/login/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import { Box, Container, Typography } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { GetStaticPropsContext } from "next";
+import LoginAccordion from "@/components/LoginAccordion";
+
+export default function Login() {
+  const t = useTranslations();
+
+  return (
+    <>
+      <Head>
+        <title>{t("Login")}</title>
+      </Head>
+
+      <Container>
+        <Box py={5}>
+          <Typography variant="h1" fontWeight={600} my={6} textAlign={"center"}>
+            {t("Login to your personal account")}
+          </Typography>
+          <LoginAccordion />
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: (await import(`locales/${context.locale}/common.json`)).default,
+    },
+  };
+}
