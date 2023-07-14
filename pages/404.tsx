398924cce2ba7db0c2df9b4ec5d45eabaabd9ca4
Nurseit <nurseitbakytbekov@gmail.com>
2023-07-14 16:09:44 +0600

    feat: create 404 not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,23 @@
-import { Container } from '@mui/material';
-import { GetStaticPropsContext } from 'next';
+import { Container } from "@mui/material";
+import { GetStaticPropsContext } from "next";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Error404() {
+  const t = useTranslations();
+
   return (
     <Container>
-      <h1>404</h1>
+      <div className="error__container">
+        <Image src="/icons/notFound.svg" alt="Not found" width={500} height={323} className="error__img" />
+        <p className="error__status">404</p>
+        <p className="error__title">{t("The page is not available")}</p>
+        <p className="error__subtitle">{t("Unfortunately, we cannot find the requested page")}</p>
+        <Link href="/" className="error__return__button">
+          {t("Go back to the main page")}
+        </Link>
+      </div>
     </Container>
   );
 }
