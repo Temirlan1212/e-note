b2d0ed7e3ef8da74b31928072b20372991b59e1f
wingmeter <nursb189@gmail.com>
2023-07-14 16:47:59 +0600

    feat: fixed

--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -1,29 +1,16 @@
 import React, { forwardRef } from "react";
 import { CheckboxProps, Checkbox as MUICheckbox } from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
-
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import CheckBoxSharpIcon from "@mui/icons-material/CheckBoxSharp";
-
-import CheckBoxOutlineBlankSharpIcon from "@mui/icons-material/CheckBoxOutlineBlankSharp";
-import RadioButtonUncheckedSharpIcon from "@mui/icons-material/RadioButtonUncheckedSharp";
-
 interface ICheckboxProps extends Omit<CheckboxProps, "ref"> {
   label?: string;
-  checkboxType?: "primary" | "secondary";
 }
 
-const Checkbox: React.ForwardRefRenderFunction<HTMLInputElement, ICheckboxProps> = (
-  { label, checkboxType = "primary", ...props },
-  ref
-) => {
+const Checkbox: React.ForwardRefRenderFunction<HTMLInputElement, ICheckboxProps> = ({ label, ...props }, ref) => {
   return (
     <FormControlLabel
       control={
         <MUICheckbox
           {...props}
-          icon={checkboxType === "secondary" ? <RadioButtonUncheckedSharpIcon /> : <CheckBoxOutlineBlankSharpIcon />}
-          checkedIcon={checkboxType === "secondary" ? <CheckCircleIcon /> : <CheckBoxSharpIcon />}
           sx={{
             color: "#1baa75",
             "&.Mui-checked": {
