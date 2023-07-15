7bc21ecdf012c3aad0f1155b09f03a04e8efc566
wingmeter <nursb189@gmail.com>
2023-07-15 23:04:51 +0600

    UI SearchBar Task 27

---
+++ components/ui/SearchBar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { UseFormRegister } from "react-hook-form";
+import { Box } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
+
+import Input from "./Input";
+import Button from "./Button";
+import { useTranslations } from "next-intl";
+
+interface ISearchBarProps {
+  register?: UseFormRegister<any>;
+  name?: string;
+  sxBox?: any;
+  loading?: boolean;
+  error?: any;
+  helperText?: any;
+}
+
+const SearchBar: React.FC<ISearchBarProps> = (props) => {
+  const t = useTranslations();
+
+  const { register, name, loading, error, helperText, ...rest } = props;
+
+  return (
+    <Box display="flex" {...rest}>
+      <Input
+        fullWidth
+        name={name}
+        placeholder={t("Search")}
+        register={register}
+        error={error}
+        helperText={helperText}
+      />
+      <Button sx={{ width: "80px" }} type="submit" color="success" loading={loading}>
+        <SearchOutlined />
+      </Button>
+    </Box>
+  );
+};
+
+export default SearchBar;
