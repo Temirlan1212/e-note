8035eec4537b6aca45daf4a7ddf12feb3f4893fa
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2023-07-17 12:36:06 +0600

    добавил register к radio, select ui компонентам

--- components/ui/Radio.tsx
+++ components/ui/Radio.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { RadioProps, Radio as MUIRadio } from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { UseFormRegister } from "react-hook-form";
 
 interface IRadioProps extends RadioProps {
   label: string;
+  register?: UseFormRegister<any>;
 }
 
-const Radio: React.FC<IRadioProps> = ({ label, ...props }) => {
+const Radio: React.FC<IRadioProps> = ({ label, register, name, ...props }) => {
   return (
     <FormControlLabel
+      {...(register && name && register(name))}
       control={
         <MUIRadio
           {...props}
