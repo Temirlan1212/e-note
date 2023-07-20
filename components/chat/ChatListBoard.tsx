a09c47be5725fa29fc7ffc80afafedfe280a7200
wingmeter <nursb189@gmail.com>
2023-07-20 17:24:51 +0600

    feat:

--- components/chat/ChatListBoard.tsx
+++ components/chat/ChatListBoard.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+import { Box, Typography } from "@mui/material";
+
 interface IChatListBoardProps {}
 
 const ChatListBoard = (props: IChatListBoardProps) => {
-  return <div>ChatListBoard</div>;
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          padding: "16px",
+          background: "#F6F6F6",
+          minWidth: "320px",
+          height: "64px",
+        }}
+      >
+        <Typography
+          sx={{
+            color: "#24334B",
+            fontSize: "18px",
+            fontWeight: "600",
+          }}
+        >
+          Ваши сообщения
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChatListBoard;
