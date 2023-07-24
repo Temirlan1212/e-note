f6a5718fc6d79070acb06c05a7c10a4d2b68e4ac
wingmeter <nursb189@gmail.com>
2023-07-24 11:28:41 +0600

    feat: add locales

---
+++ pages/user-registry/index.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import { Container } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { GetStaticPropsContext } from "next";
+
+export default function UserRegistry() {
+  const t = useTranslations();
+
+  return (
+    <>
+      <Head>
+        <title>{t("User Registry")}</title>
+      </Head>
+
+      <Container component="section" maxWidth="xl">
+        asdas
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: {
+        ...(await import(`locales/${context.locale}/common.json`)).default,
+        ...(await import(`locales/${context.locale}/user-registry.json`)).default,
+      },
+    },
+  };
+}
