235ec7f0672c2a1e7048fd2ef260b54c735641d6
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2023-07-24 10:31:24 +0600

    Добавить modal

---
+++ components/ui/Modal.tsx
@@ -0,0 +1,24 @@
+import { SxProps, Theme } from "@mui/material";
+import Box from "@mui/material/Box";
+import MUIModal, { ModalProps } from "@mui/material/Modal";
+
+const contentStyle: SxProps<Theme> = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: 300, sm: 400 },
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: { xs: 1.5, sm: 4 },
+};
+
+interface IModalProps extends ModalProps {}
+
+export default function Modal({ children, ...rest }: IModalProps) {
+  return (
+    <MUIModal aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" {...rest}>
+      <Box sx={contentStyle}>{children}</Box>
+    </MUIModal>
+  );
+}
