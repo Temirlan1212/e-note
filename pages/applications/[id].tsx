fb137a03424cd800898fa9794b55c93b5da479d6
Alymbek Alisher uulu <alymbek1992@gmail.com>
2023-07-25 19:15:59 +0600

    47. Сформировать страницы для создания заявки

---
+++ pages/applications/[id].tsx
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useTranslations } from "next-intl";
+import { GetStaticPathsContext, GetStaticPropsContext } from "next";
+import { Box, Container, Typography } from "@mui/material";
+import ApplicationForm from "@/components/applications/form/ApplicationForm";
+import Button from "@/components/ui/Button";
+import CloseIcon from "@mui/icons-material/Close";
+
+export default function Applications() {
+  const router = useRouter();
+  const t = useTranslations();
+
+  const handleCancelClick = () => {
+    router.push("/applications");
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{t("Applications")}</title>
+      </Head>
+
+      <Container maxWidth="xl">
+        <Box display="flex" alignItems="center" justifyContent="space-between" my={2}>
+          <Typography variant="h4" color="success.main">
+            {t("Applications")}
+          </Typography>
+          <Box>
+            <Button buttonType="secondary" startIcon={<CloseIcon />} onClick={handleCancelClick}>
+              {t("Cancel")}
+            </Button>
+          </Box>
+        </Box>
+        <ApplicationForm />
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: {
+        ...(await import(`locales/${context.locale}/common.json`)).default,
+        ...(await import(`locales/${context.locale}/validator.json`)).default,
+        ...(await import(`locales/${context.locale}/applications.json`)).default,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths(context: GetStaticPathsContext) {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
