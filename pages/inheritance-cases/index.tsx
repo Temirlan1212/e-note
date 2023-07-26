73850b93290ff344d847f62dcc9d8a91fde5db5f
Nurseit <nurseitbakytbekov@gmail.com>
2023-07-26 17:29:12 +0600

    изменил способ импорта и паддинг

--- pages/inheritance-cases/index.tsx
+++ pages/inheritance-cases/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Container } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { GetStaticPropsContext } from "next";
-import InheritanceCases from "../../components/inheritance-cases/InheritanceCases";
+import InheritanceCases from "@/components/inheritance-cases/InheritanceCases";
 
 export default function inheritanceCases() {
   const t = useTranslations();
@@ -17,7 +17,7 @@ export default function inheritanceCases() {
           display: "flex",
           flexDirection: "column",
           gap: "30px",
-          padding: { xs: "30px 20px 31px 20px", sm: "30px 20px 57px 20px", md: "60px 60px 198px 60px" },
+          padding: { xs: "30px 20px 25px 20px", sm: "30px 20px 34px 20px", md: "60px 60px 124px 60px" },
           maxWidth: { xs: "unset", sm: "unset", md: "unset", lg: "unset" },
         }}
       >
