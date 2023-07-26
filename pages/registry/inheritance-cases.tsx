7c1f76566859794b574448e7cbc1fc38627416c3
Nurseit <nurseitbakytbekov@gmail.com>
2023-07-26 15:18:18 +0600

    Страница реестра наследственных дел

---
+++ pages/registry/inheritance-cases.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { Container } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { GetStaticPropsContext } from "next";
+import InheritanceCases from "../../components/registry/InheritanceCases";
+
+export default function inheritanceCases() {
+  const t = useTranslations();
+
+  return (
+    <>
+      <Head>
+        <title>{t("Register of inheritance cases")}</title>
+      </Head>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          padding: { xs: "30px 20px 31px 20px", sm: "30px 20px 57px 20px", md: "60px 60px 198px 60px" },
+          maxWidth: { xs: "unset", sm: "unset", md: "unset", lg: "unset" },
+        }}
+      >
+        <InheritanceCases />
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: {
+        ...(await import(`locales/${context.locale}/common.json`)).default,
+        ...(await import(`locales/${context.locale}/inheritance-cases.json`)).default,
+      },
+    },
+  };
+}
