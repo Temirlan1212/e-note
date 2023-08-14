46df5f46fbc6b00b555e0751e3027510f03c1101
wingmeter <nursb189@gmail.com>
2023-08-14 11:43:35 +0600

    fixed

--- pages/api/profile/user-data.ts
+++ pages/api/profile/user-data.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       Cookie: req.headers["server-cookie"]?.toString() ?? "",
     },
     body: JSON.stringify({
-      fields: ["email", "name", "partner.mobilePhone", "partner", "code", "image", "roles"],
+      fields: ["email", "name", "partner.mobilePhone", "partner", "group", "code", "image", "roles"],
       data: {
         criteria: [{ fieldName: "code", operator: "=", value: req.body.username }],
       },
