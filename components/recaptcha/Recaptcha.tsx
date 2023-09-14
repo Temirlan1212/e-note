70540e19cb4d13353783a058afcdb769d92dfc82
Nurseit <nurseitbakytbekov@gmail.com>
2023-09-14 11:48:20 +0600

    добавил в env.local.example, беру оттуда ключ

--- components/recaptcha/Recaptcha.tsx
+++ components/recaptcha/Recaptcha.tsx
@@ -1,28 +1,39 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useRouter } from "next/router";
 import ReCAPTCHA from "react-google-recaptcha";
 import useFetch from "@/hooks/useFetch";
+import useEffectOnce from "@/hooks/useEffectOnce";
 
 interface IRecaptchaProps {
   onRecaptchaSuccess: (success: boolean) => void;
 }
 const Recaptcha: React.FC<IRecaptchaProps> = ({ onRecaptchaSuccess }) => {
   const recaptchaRef = useRef<ReCAPTCHA | null>(null);
+  const [publicSiteKey, setPublicSiteKey] = useState<string | null>(null);
   const { locale } = useRouter();
 
   const { update } = useFetch("", "POST");
+  const { data: getKeyData, update: getKey } = useFetch("", "POST");
 
   const handleRecaptchaChange = async (token: string | null) => {
     await update("/api/recaptcha", { token: token }).then((data) => onRecaptchaSuccess(data.success));
   };
 
+  useEffectOnce(async () => {
+    await getKey("/api/recaptcha", { requestType: "getPublicKey" }).then((getKeyData) => setPublicSiteKey(getKeyData));
+  });
+
   return (
-    <ReCAPTCHA
-      sitekey="6LdZVBwoAAAAAFjhQbUlEdfpyrz5HT9LPQyHhfGr"
-      ref={recaptchaRef}
-      hl={locale === "kg" ? "ru" : locale}
-      onChange={handleRecaptchaChange}
-    />
+    <>
+      {publicSiteKey && (
+        <ReCAPTCHA
+          sitekey={publicSiteKey as string}
+          ref={recaptchaRef}
+          hl={locale === "kg" ? "ru" : locale}
+          onChange={handleRecaptchaChange}
+        />
+      )}
+    </>
   );
 };
 
