a4a91e8b6cc03f02c9236c2b1713138f316ec685
ilimKUSH <ilimKUSH>
2023-09-27 11:13:41 +0600

    202. Поменял валидацию у ИНН

--- validator-schemas/person.ts
+++ validator-schemas/person.ts
@@ -27,7 +27,7 @@ export const personSchema = object()
       .matches(/^[aA-zZаА-яЯөүңӨҮҢ\s]*$/, "onlyLetters"),
     personalNumber: string()
       .trim()
-      .min(6, "minNumbers")
+      .min(14, "minNumbers")
       .required("required")
       .matches(/^[0-9]*$/, "onlyNumbers"),
     birthDate: date().nullable(),
