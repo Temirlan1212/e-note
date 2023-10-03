bc6428c3aa8418c37f694df3c6666179fd894bfd
Nurseit <nurseitbakytbekov@gmail.com>
2023-10-03 17:38:57 +0600

    исправил ошибку, добавил маркеры

--- pages/notary-registry/index.tsx
+++ pages/notary-registry/index.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
 import dynamic from "next/dynamic";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, CircularProgress, Container, Typography } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { GetStaticPropsContext } from "next";
-import NotaryRegistry from "@/components/notary-registry/NotaryRegistry";
-
-const center: [number, number] = [42.882004, 74.582748];
+import NotariesContent from "@/components/notaries/NotariesContent";
+import useFetch from "@/hooks/useFetch";
+import { useState } from "react";
+import useEffectOnce from "@/hooks/useEffectOnce";
+import { IMarker } from "@/components/ui/LeafletMap";
 
 export default function Notaries() {
   const t = useTranslations();
+
+  const [markers, setMarkers] = useState<IMarker[]>();
 
   const LeafletMap = dynamic(
     () => {
@@ -17,23 +21,60 @@ export default function Notaries() {
     { loading: () => <p>Loading...</p>, ssr: false }
   );
 
+  const { data: notaryData, loading: notaryDataLoading } = useFetch("/api/notaries", "POST");
+
+  useEffectOnce(() => {
+    if (Array.isArray(notaryData?.data)) {
+      setMarkers(
+        notaryData?.data.map((item) => ({
+          coordinates: {
+            lat: item.latitude,
+            lng: item.longitude,
+          },
+          popup: (
+            <>
+              <Typography fontSize={{ xs: 9, sm: 10, md: 12, lg: 14 }} fontWeight={600}>
+                {item?.name}
+              </Typography>
+              <Typography fontSize={{ xs: 9, sm: 10, md: 12, lg: 14 }} fontWeight={500}>
+                {item?.["address.fullName"]}
+              </Typography>
+            </>
+          ),
+        }))
+      );
+    }
+  }, [notaryData]);
+
   return (
     <>
       <Head>
         <title>{t("Notaries")}</title>
       </Head>
 
-      <Container maxWidth="xl" sx={{ py: "30px" }}>
-        <NotaryRegistry />
-        <Box paddingBottom="80px" display="flex" flexDirection="column" gap="20px">
-          <Typography variant="h4">{t("Search for a notary on the map")}</Typography>
-          <LeafletMap
-            center={center}
-            zoom={12}
-            style={{
-              height: "600px",
+      <Container>
+        <NotariesContent />
+        <Box paddingBottom="80px" display="flex" flexDirection="column" gap="50px">
+          <Typography
+            component="h1"
+            sx={{
+              fontSize: "36px",
+              fontWeight: 600,
             }}
-          />
+          >
+            {t("Search for a notary on the map")}
+          </Typography>
+          {!notaryDataLoading && notaryData ? (
+            <LeafletMap
+              zoom={12}
+              markers={markers}
+              style={{
+                height: "600px",
+              }}
+            />
+          ) : (
+            <CircularProgress />
+          )}
         </Box>
       </Container>
     </>
