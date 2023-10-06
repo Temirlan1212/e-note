6677b020e9a1ecbd79e2f84c81f0d642ba70f095
Alymbek Alisher uulu <alymbek1992@gmail.com>
2023-10-06 17:17:57 +0600

    216. Добавить возможность подписи через Рутокен

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,16 +10,6 @@ export default function Document(props: DocumentProps) {
         <Main />
         <NextScript />
 
-        <Script id="JCWebClient" strategy="afterInteractive">
-          {`(function (url = "https://localhost:24738/JCWebClient.js") {
-              var parent = document.getElementsByTagName("body")[0];
-              var script = document.createElement("script");
-              script.type = "text/javascript";
-              script.src = url;
-              parent.appendChild(script);
-            })();`}
-        </Script>
-
         <Script id="YandexMetrika" strategy="afterInteractive">
           {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
