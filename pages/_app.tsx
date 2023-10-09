37a36f3e784144f0daaa1bb9861fb3c40661a61a
ilimKUSH <ilimKUSH>
2023-10-09 14:46:01 +0600

    239. Теперь переходит обратно когда не авторизован

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ function Layout({ children }: { children: JSX.Element }) {
     switch (router.asPath) {
       case "/login":
         if (profile.user != null)
-          if (previousPath != null && !isRoutesIncludesPath(guestRoutes, previousPath)) {
+          if (previousPath != null && isRoutesIncludesPath(guestRoutes, previousPath)) {
             router.push(previousPath);
           } else {
             router.push("/applications");
