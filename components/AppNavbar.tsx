b9df705e087642c8863d33209910eb57a5a96ca0
Alymbek Alisher uulu <alymbek1992@gmail.com>
2023-10-25 19:08:11 +0600

    fix: 25.10.2023 19:08

--- components/AppNavbar.tsx
+++ components/AppNavbar.tsx
@@ -178,10 +178,12 @@ export default function AppNavbar({ children, type, routes }: IAppNavbarProps) {
           }}
         >
           <Box sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
-            <Image src="/images/logo.png" alt="E-notariat" width={48} height={48} />
-            <Typography variant="h6" color="text.primary" fontWeight={600} whiteSpace="nowrap">
-              E-NOTARIAT
-            </Typography>
+            <Link href="/" sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <Image src="/images/logo.png" alt="E-notariat" width={48} height={48} />
+              <Typography variant="h6" color="text.primary" fontWeight={600} whiteSpace="nowrap">
+                E-NOTARIAT
+              </Typography>
+            </Link>
           </Box>
 
           {type === "public" && (
