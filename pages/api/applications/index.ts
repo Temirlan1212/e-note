47fd07d936829e48a787354bfd509bd4e475d7fa
Nurseit <nurseitbakytbekov@gmail.com>
2023-10-27 13:30:00 +0600

    из респонса приходит только id

--- pages/api/applications/index.ts
+++ pages/api/applications/index.ts
@@ -86,7 +86,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         "notaryUniqNumber",
         "members.fullName",
         "requester.fullName",
-        "barCode",
+        "barCode.id",
       ],
       sortBy: req.body["sortBy"] ?? [],
       data: {
