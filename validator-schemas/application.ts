157509f9be3fa3309aac260dee38c5a398f5be1a
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2023-10-27 11:50:19 +0600

    Убрать tundukPersonalNumber

--- validator-schemas/application.ts
+++ validator-schemas/application.ts
@@ -52,6 +52,7 @@ export const applicationSchema = object()
       .nullable()
       .default(null)
       .test("nullable-required", "required", (v) => v != null),
+
     requester: array().of(personSchema),
     members: array().of(
       personSchema.concat(
@@ -64,7 +65,5 @@ export const applicationSchema = object()
         })
       )
     ),
-    notaryUnilateralAction: boolean(),
-    selectedStepByTab: number(),
   })
   .concat(addressSchema.pick(["region", "district", "city"]));
