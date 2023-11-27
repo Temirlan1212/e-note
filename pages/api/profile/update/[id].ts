8ec0fa8616750b1121e3bda0c3e54c1c618d9259
Ilim <ikusbekov@gmail.com>
2023-11-27 14:04:09 +0300

    profilePass

--- pages/api/profile/update/[id].ts
+++ pages/api/profile/update/[id].ts
@@ -7,54 +7,59 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json(null);
   }
 
-  const { submitData, userData, imageBase64, userRole } = req.body.body;
+  const buildRequestBody = () => {
+    if (req.body.body && req.body.body.submitData) {
+      const { submitData, userData, imageBase64, userRole } = req.body.body;
 
-  const requestBody: any = {
-    id: userData?.data?.[0]?.id,
-    version: userData?.data?.[0]?.version,
-    image: imageBase64,
-    partner: {
-      id: userData?.data?.[0]?.partner?.id,
-      version: userData?.data?.[0]?.partner?.version,
-      firstName: submitData?.partner?.firstName,
-      lastName: submitData?.partner?.lastName,
-      middleName: submitData?.partner?.middleName,
-      mobilePhone: submitData?.partner?.mobilePhone,
-      emailAddress: {
-        id: userData?.data?.[0]?.partner?.emailAddress?.id,
-        version: userData?.data?.[0]?.partner?.emailAddress?.$version,
-        address: submitData?.partner?.emailAddress.address,
-      },
-    },
+      const requestBody: any = {
+        id: userData?.data?.[0]?.id,
+        version: userData?.data?.[0]?.version,
+        image: imageBase64,
+        partner: {
+          id: userData?.data?.[0]?.partner?.id,
+          version: userData?.data?.[0]?.partner?.version,
+          firstName: submitData?.partner?.firstName,
+          lastName: submitData?.partner?.lastName,
+          middleName: submitData?.partner?.middleName,
+          mobilePhone: submitData?.partner?.mobilePhone,
+          emailAddress: {
+            id: userData?.data?.[0]?.partner?.emailAddress?.id,
+            version: userData?.data?.[0]?.partner?.emailAddress?.$version,
+            address: submitData?.partner?.emailAddress.address,
+          },
+        },
+      };
+
+      if (userRole === "notary") {
+        requestBody.activeCompany = {
+          id: userData?.data?.[0]?.activeCompany?.id,
+          version: userData?.data?.[0]?.activeCompany?.version,
+          longitude: submitData?.activeCompany?.longitude,
+          latitude: submitData?.activeCompany?.latitude,
+          address: {
+            id: userData?.data?.[0]?.activeCompany?.address?.id,
+            version: userData?.data?.[0]?.activeCompany?.address?.$version,
+            region: {
+              id: submitData?.activeCompany?.address?.region?.id,
+            },
+            city: {
+              id: submitData?.activeCompany?.address?.city?.id,
+            },
+            district: {
+              id: submitData?.activeCompany?.address?.district?.id,
+            },
+            addressL4: submitData?.activeCompany?.address?.addressL4,
+            addressL3: submitData?.activeCompany?.address?.addressL3,
+            addressL2: submitData?.activeCompany?.address?.addressL2,
+          },
+          notaryDistrict: {
+            id: submitData?.activeCompany?.notaryDistrict?.id,
+          },
+        };
+      }
+      return requestBody;
+    }
   };
-
-  if (userRole === "notary") {
-    requestBody.activeCompany = {
-      id: userData?.data?.[0]?.activeCompany?.id,
-      version: userData?.data?.[0]?.activeCompany?.version,
-      longitude: submitData?.activeCompany?.longitude,
-      latitude: submitData?.activeCompany?.latitude,
-      address: {
-        id: userData?.data?.[0]?.activeCompany?.address?.id,
-        version: userData?.data?.[0]?.activeCompany?.address?.$version,
-        region: {
-          id: submitData?.activeCompany?.address?.region?.id,
-        },
-        city: {
-          id: submitData?.activeCompany?.address?.city?.id,
-        },
-        district: {
-          id: submitData?.activeCompany?.address?.district?.id,
-        },
-        addressL4: submitData?.activeCompany?.address?.addressL4,
-        addressL3: submitData?.activeCompany?.address?.addressL3,
-        addressL2: submitData?.activeCompany?.address?.addressL2,
-      },
-      notaryDistrict: {
-        id: submitData?.activeCompany?.notaryDistrict?.id,
-      },
-    };
-  }
 
   const response = await fetch(process.env.BACKEND_API_URL + `/ws/rest/com.axelor.auth.db.User/${id}`, {
     method: req.method,
@@ -63,7 +68,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       Cookie: req.headers["server-cookie"]?.toString() ?? "",
     },
     body: JSON.stringify({
-      data: requestBody,
+      data: buildRequestBody() ? buildRequestBody() : { ...req.body },
     }),
   });
 
