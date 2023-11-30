92a38000154f847530f439c50978ed12da689adb
Ilim <ikusbekov@gmail.com>
2023-11-30 14:31:24 +0600

    405. Страница аналитики и юр лицо фикс

---
+++ pages/api/analytics/[date].ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { date } = req.query;
+  if (req.method !== "POST") {
+    return res.status(400).json(null);
+  }
+
+  const response = await fetch(process.env.BACKEND_API_URL + `/ws/chart/${date}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: req.headers["server-cookie"]?.toString() ?? "",
+    },
+  });
+
+  if (!response.ok) {
+    return res.status(response.status).json(null);
+  }
+
+  const responseData = await response.json();
+
+  return res.status(200).json(responseData);
+}
