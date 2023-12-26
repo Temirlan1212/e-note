ace6058667843c4563a9fa161063b19420d72d56
Ilim <ikusbekov@gmail.com>
2023-12-26 16:18:38 +0600

    479. фильтрация нот действий перенести поля в два столбика

--- components/fields/NotarialAction.tsx
+++ components/fields/NotarialAction.tsx
@@ -190,196 +190,223 @@ export default function NotarialAction({ form, step }: INotarialActionProps) {
       />
 
       <ExpandingFields title="Additional filters" permanentExpand={isAdditionalFieldsOpen}>
-        <Box display="flex" flexDirection="column" gap="10px">
-          <Typography color="secondary" variant="h4">
-            {t("Filter by object")}
-          </Typography>
+        <Hint type="hint" defaultActive={false}>
+          {t("second-step-hint-title")}
+        </Hint>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "end",
+            gap: "20px",
+            flexWrap: "wrap",
+            marginTop: "20px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              width: { xs: "100%", md: "40%" },
+            }}
+          >
+            <Typography color="secondary" variant="h4">
+              {t("Filter by object")}
+            </Typography>
 
-          <Controller
-            control={control}
-            name="object"
-            defaultValue={null}
-            render={({ field, fieldState }) => {
-              const objectList = objectData?.data;
-              const errorMessage = fieldState.error?.message;
-              return (
-                <Box width="100%" display="flex" flexDirection="column" gap="10px">
-                  <Box display="flex" flexWrap="wrap" justifyContent="space-between" gap="10px 20px" alignItems="end">
-                    <InputLabel sx={{ fontWeight: 600 }}>{t("Objects of civil rights")}</InputLabel>
-                    <Hint type="hint" maxWidth="520px" defaultActive={false}>
-                      {t("second-step-hint-title")}
-                    </Hint>
+            <Controller
+              control={control}
+              name="object"
+              defaultValue={null}
+              render={({ field, fieldState }) => {
+                const objectList = objectData?.data;
+                const errorMessage = fieldState.error?.message;
+                return (
+                  <Box width="100%" display="flex" flexDirection="column" gap="10px">
+                    <Box display="flex" flexWrap="wrap" justifyContent="space-between" gap="10px 20px" alignItems="end">
+                      <InputLabel sx={{ fontWeight: 600 }}>{t("Objects of civil rights")}</InputLabel>
+                    </Box>
+
+                    <Select
+                      sx={{ fontWeight: 500 }}
+                      fullWidth
+                      placeholder={t("All objects of civil rights")}
+                      selectType={errorMessage ? "error" : field.value ? "success" : "secondary"}
+                      data={objectList ?? []}
+                      labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
+                      valueField="id"
+                      helperText={errorMessage ? t(errorMessage) : ""}
+                      value={field.value == null ? "" : field.value}
+                      onBlur={field.onBlur}
+                      loading={objectLoading}
+                      onChange={(...event: any[]) => {
+                        field.onChange(...event);
+                        trigger(field.name);
+                        ["objectType", "notarialAction", "typeNotarialAction", "action", "product"].map((item: any) =>
+                          resetField(item, { defaultValue: null })
+                        );
+                      }}
+                      ref={field.ref}
+                    />
                   </Box>
+                );
+              }}
+            />
 
-                  <Select
-                    sx={{ fontWeight: 500 }}
-                    fullWidth
-                    placeholder={t("All objects of civil rights")}
-                    selectType={errorMessage ? "error" : field.value ? "success" : "secondary"}
-                    data={objectList ?? []}
-                    labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
-                    valueField="id"
-                    helperText={errorMessage ? t(errorMessage) : ""}
-                    value={field.value == null ? "" : field.value}
-                    onBlur={field.onBlur}
-                    loading={objectLoading}
-                    onChange={(...event: any[]) => {
-                      field.onChange(...event);
-                      trigger(field.name);
-                      ["objectType", "notarialAction", "typeNotarialAction", "action", "product"].map((item: any) =>
-                        resetField(item, { defaultValue: null })
-                      );
-                    }}
-                    ref={field.ref}
-                  />
-                </Box>
-              );
+            <Controller
+              control={control}
+              name="objectType"
+              defaultValue={null}
+              render={({ field, fieldState }) => {
+                const errorMessage = fieldState.error?.message;
+                const objectTypeList = objectTypeData?.data;
+                return (
+                  <Box width="100%" display="flex" flexDirection="column" gap="10px">
+                    <InputLabel sx={{ fontWeight: 600 }}>{t("Object type")}</InputLabel>
+                    <Select
+                      sx={{ fontWeight: 500 }}
+                      placeholder={t("All types of objects")}
+                      selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
+                      data={objectTypeList ?? []}
+                      labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
+                      valueField="id"
+                      helperText={!!objectVal && errorMessage ? t(errorMessage) : ""}
+                      value={field.value == null ? "" : field.value}
+                      onBlur={field.onBlur}
+                      loading={objectTypeLoading}
+                      onChange={(...event: any[]) => {
+                        field.onChange(...event);
+                        trigger(field.name);
+                        ["notarialAction", "typeNotarialAction", "action", "product"].map((item: any) =>
+                          resetField(item, { defaultValue: null })
+                        );
+                      }}
+                      ref={field.ref}
+                    />
+                  </Box>
+                );
+              }}
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              width: { xs: "100%", md: "55%" },
             }}
-          />
+          >
+            <Typography color="secondary" variant="h4">
+              {t("Filter by document")}
+            </Typography>
 
-          <Controller
-            control={control}
-            name="objectType"
-            defaultValue={null}
-            render={({ field, fieldState }) => {
-              const errorMessage = fieldState.error?.message;
-              const objectTypeList = objectTypeData?.data;
-              return (
-                <Box width="100%" display="flex" flexDirection="column" gap="10px">
-                  <InputLabel sx={{ fontWeight: 600 }}>{t("Object type")}</InputLabel>
-                  <Select
-                    sx={{ fontWeight: 500 }}
-                    placeholder={t("All types of objects")}
-                    selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
-                    data={objectTypeList ?? []}
-                    labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
-                    valueField="id"
-                    helperText={!!objectVal && errorMessage ? t(errorMessage) : ""}
-                    value={field.value == null ? "" : field.value}
-                    onBlur={field.onBlur}
-                    loading={objectTypeLoading}
-                    onChange={(...event: any[]) => {
-                      field.onChange(...event);
-                      trigger(field.name);
-                      ["notarialAction", "typeNotarialAction", "action", "product"].map((item: any) =>
-                        resetField(item, { defaultValue: null })
-                      );
-                    }}
-                    ref={field.ref}
-                  />
-                </Box>
-              );
-            }}
-          />
+            <Controller
+              control={control}
+              name="notarialAction"
+              defaultValue={null}
+              render={({ field, fieldState }) => {
+                const errorMessage = fieldState.error?.message;
+                const notarialActionList = notarialActionData?.data;
 
-          <Typography color="secondary" variant="h4" mt="20px">
-            {t("Filter by document")}
-          </Typography>
+                return (
+                  <Box width="100%" display="flex" flexDirection="column" gap="10px">
+                    <InputLabel sx={{ fontWeight: 600 }}>{t("Notarial action")}</InputLabel>
+                    <Select
+                      sx={{ fontWeight: 500 }}
+                      placeholder={t("All notarial actions")}
+                      selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
+                      data={notarialActionList ?? []}
+                      labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
+                      valueField="id"
+                      helperText={!!objectTypeVal && errorMessage ? t(errorMessage) : ""}
+                      value={field.value == null ? "" : field.value}
+                      onBlur={field.onBlur}
+                      loading={notarialActionLoading}
+                      onChange={(...event: any[]) => {
+                        field.onChange(...event);
+                        trigger(field.name);
+                        ["typeNotarialAction", "action", "product"].map((item: any) =>
+                          resetField(item, { defaultValue: null })
+                        );
+                      }}
+                      ref={field.ref}
+                    />
+                  </Box>
+                );
+              }}
+            />
 
-          <Controller
-            control={control}
-            name="notarialAction"
-            defaultValue={null}
-            render={({ field, fieldState }) => {
-              const errorMessage = fieldState.error?.message;
-              const notarialActionList = notarialActionData?.data;
+            <Controller
+              control={control}
+              name="typeNotarialAction"
+              defaultValue={null}
+              render={({ field, fieldState }) => {
+                const errorMessage = fieldState.error?.message;
+                const typeNotarialActionList = typeNotarialActionData?.data;
 
-              return (
-                <Box width="100%" display="flex" flexDirection="column" gap="10px">
-                  <InputLabel sx={{ fontWeight: 600 }}>{t("Notarial action")}</InputLabel>
-                  <Select
-                    sx={{ fontWeight: 500 }}
-                    placeholder={t("All notarial actions")}
-                    selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
-                    data={notarialActionList ?? []}
-                    labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
-                    valueField="id"
-                    helperText={!!objectTypeVal && errorMessage ? t(errorMessage) : ""}
-                    value={field.value == null ? "" : field.value}
-                    onBlur={field.onBlur}
-                    loading={notarialActionLoading}
-                    onChange={(...event: any[]) => {
-                      field.onChange(...event);
-                      trigger(field.name);
-                      ["typeNotarialAction", "action", "product"].map((item: any) =>
-                        resetField(item, { defaultValue: null })
-                      );
-                    }}
-                    ref={field.ref}
-                  />
-                </Box>
-              );
-            }}
-          />
-
-          <Controller
-            control={control}
-            name="typeNotarialAction"
-            defaultValue={null}
-            render={({ field, fieldState }) => {
-              const errorMessage = fieldState.error?.message;
-              const typeNotarialActionList = typeNotarialActionData?.data;
-
-              return (
-                <Box width="100%" display="flex" flexDirection="column" gap="10px">
-                  <InputLabel sx={{ fontWeight: 600 }}>{t("Type of notarial action")}</InputLabel>
-                  <Select
-                    sx={{ fontWeight: 500 }}
-                    placeholder={t("All types of notarial actions")}
-                    selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
-                    data={typeNotarialActionList ?? []}
-                    labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
-                    valueField="id"
-                    helperText={!!notarialActionVal && errorMessage ? t(errorMessage) : ""}
-                    value={field.value == null ? "" : field.value}
-                    onBlur={field.onBlur}
-                    loading={typeNotarialActionLoading}
-                    onChange={(...event: any[]) => {
-                      field.onChange(...event);
-                      trigger(field.name);
-                      ["action", "product"].map((item: any) => resetField(item, { defaultValue: null }));
-                    }}
-                    ref={field.ref}
-                  />
-                </Box>
-              );
-            }}
-          />
-
-          <Controller
-            control={control}
-            name="action"
-            defaultValue={null}
-            render={({ field, fieldState }) => {
-              const errorMessage = fieldState.error?.message;
-              const actionList = actionData?.data;
-
-              return (
-                <Box width="100%" display="flex" flexDirection="column" gap="10px">
-                  <InputLabel sx={{ fontWeight: 600 }}>{t("Purpose of action")}</InputLabel>
-                  <Select
-                    sx={{ fontWeight: 500 }}
-                    placeholder={t("All action goals")}
-                    selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
-                    data={actionList ?? []}
-                    labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
-                    valueField="id"
-                    helperText={!!typeNotarialActionVal && errorMessage ? t(errorMessage) : ""}
-                    value={field.value == null ? "" : field.value}
-                    onBlur={field.onBlur}
-                    loading={actionLoading}
-                    onChange={(...event: any[]) => {
-                      field.onChange(...event);
-                      trigger(field.name);
-                      resetField("product", { defaultValue: null });
-                    }}
-                    ref={field.ref}
-                  />
-                </Box>
-              );
-            }}
-          />
+                return (
+                  <Box width="100%" display="flex" flexDirection="column" gap="10px">
+                    <InputLabel sx={{ fontWeight: 600 }}>{t("Type of notarial action")}</InputLabel>
+                    <Select
+                      sx={{ fontWeight: 500 }}
+                      placeholder={t("All types of notarial actions")}
+                      selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
+                      data={typeNotarialActionList ?? []}
+                      labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
+                      valueField="id"
+                      helperText={!!notarialActionVal && errorMessage ? t(errorMessage) : ""}
+                      value={field.value == null ? "" : field.value}
+                      onBlur={field.onBlur}
+                      loading={typeNotarialActionLoading}
+                      onChange={(...event: any[]) => {
+                        field.onChange(...event);
+                        trigger(field.name);
+                        ["action", "product"].map((item: any) => resetField(item, { defaultValue: null }));
+                      }}
+                      ref={field.ref}
+                    />
+                  </Box>
+                );
+              }}
+            />
+          </Box>
         </Box>
+
+        <Controller
+          control={control}
+          name="action"
+          defaultValue={null}
+          render={({ field, fieldState }) => {
+            const errorMessage = fieldState.error?.message;
+            const actionList = actionData?.data;
+
+            return (
+              <Box width="100%" display="flex" flexDirection="column" gap="10px" marginTop="10px">
+                <InputLabel sx={{ fontWeight: 600 }}>{t("Purpose of action")}</InputLabel>
+                <Select
+                  sx={{ fontWeight: 500 }}
+                  placeholder={t("All action goals")}
+                  selectType={fieldState.error?.message ? "error" : field.value ? "success" : "secondary"}
+                  data={actionList ?? []}
+                  labelField={"nameIn" + locale?.[0].toUpperCase() + locale?.slice(1)}
+                  valueField="id"
+                  helperText={!!typeNotarialActionVal && errorMessage ? t(errorMessage) : ""}
+                  value={field.value == null ? "" : field.value}
+                  onBlur={field.onBlur}
+                  loading={actionLoading}
+                  onChange={(...event: any[]) => {
+                    field.onChange(...event);
+                    trigger(field.name);
+                    resetField("product", { defaultValue: null });
+                  }}
+                  ref={field.ref}
+                />
+              </Box>
+            );
+          }}
+        />
       </ExpandingFields>
     </>
   );
