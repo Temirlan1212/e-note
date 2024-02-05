9d8da78a42fb1e6b3320419de9a09776702bcfc3
Ilim <ikusbekov@gmail.com>
2024-02-05 18:53:45 +0600

    615. Убрать валидацию у поиска по уник номеру

--- validator-schemas/check-document-by-number.ts
+++ validator-schemas/check-document-by-number.ts
@@ -4,5 +4,5 @@ import { InferType } from "yup";
 export type ICheckDocumentByNumber = InferType<typeof checkDocumentByNumber>;
 
 export const checkDocumentByNumber = yup.object().shape({
-  keyword: yup.string().matches(/^[0-9\-]*$/, "onlyNumbers"),
+  keyword: yup.string(),
 });
